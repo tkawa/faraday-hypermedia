4a53965091d39030a2d80feaa757e0e8751f9673
Toru KAWAMURA <tkawa@4bit.net>
2016-06-04 18:37:35 +0900

    Initial commit `$ bundle gem faraday-hypermedia`

---
+++ lib/faraday/hypermedia.rb
@@ -0,0 +1,7 @@
+require "faraday/hypermedia/version"
+
+module Faraday
+  module Hypermedia
+    # Your code goes here...
+  end
+end
---
+++ lib/faraday/hypermedia/version.rb
@@ -0,0 +1,5 @@
+module Faraday
+  module Hypermedia
+    VERSION = "0.1.0"
+  end
+end
